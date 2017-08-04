b3967ab76cf6fa0af91431ae5582d6eebd2c5722
Peter Keeler <peter@bonevm.com>
2017-08-04 14:05:20 -0700

    Initial commit.
    Just some basic framework stuff. Nothing too exciting yet.

---
+++ src/main/java/org/oneuponcancer/redemption/RedemptionApplication.java
@@ -0,0 +1,11 @@
+package org.oneuponcancer.redemption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedemptionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedemptionApplication.class, args);
+    }
+}
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Redemption</title>
+</head>
+<body>
+<h1>Redemption!</h1>
+</body>
+</html>
