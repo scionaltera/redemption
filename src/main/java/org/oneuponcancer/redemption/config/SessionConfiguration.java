c0f429a11958c5855f3852885ab53f96a4291efa
Peter Keeler <peter@bonevm.com>
2018-04-07 00:13:52 -0700

    Add JDBC based sessions.
    Fixes #33.

---
+++ src/main/java/org/oneuponcancer/redemption/config/SessionConfiguration.java
@@ -0,0 +1,9 @@
+package org.oneuponcancer.redemption.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@EnableJdbcHttpSession
+@Configuration
+public class SessionConfiguration {
+}
