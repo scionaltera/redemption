dfc79675c8c4aa7cc30eb4d315329a6a15bdb01a
Peter Keeler <peter@bonevm.com>
2018-10-08 21:26:35 -0700

    Switching database support to MySQL
    I'm starting to use a "serverless" AWS Aurora cluster, which has to be MySQL.

--- src/main/java/org/oneuponcancer/redemption/model/Staff.java
+++ src/main/java/org/oneuponcancer/redemption/model/Staff.java
@@ -1,7 +1,8 @@
 package org.oneuponcancer.redemption.model;
 
-import org.hibernate.annotations.Type;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -16,8 +17,9 @@ import java.util.UUID;
 @Entity
 public class Staff {
     @Id
-    @GeneratedValue
-    @Type(type = "pg-uuid")
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
     private UUID id;
 
     private String username;
